9a11b058fa7c9bb4f97d5c8bf657ca0037cae4e9
HuyNgaos <150988966+HuyNgaos@users.noreply.github.com>
2024-01-07 13:20:42 -0600

    Delete user.py

--- user.py
+++
@@ -1,9 +0,0 @@
-class USER():
-    def __init__(self, username, preference):
-        self.username = username
-        self.preference = preference
-        self.admin = False
-        if username == 'Admin' and preference == 'test':
-            self.admin = True
-        else:
-            self.admin = False
